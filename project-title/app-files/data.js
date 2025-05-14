839b3d92022fe81044832e8ce2c1217f11a6dd1f
HarvyLiu <harvyliu2009@gmail.com>
2025-05-15 02:06:21 +0800

    pano3

---
+++ project-title/app-files/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20250513_172052_2",
+      "name": "PANO_20250513_172052_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.1567779747069924,
+        "pitch": 0.19656202917945365,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20096924640681024,
+          "pitch": 0.1048696148846151,
+          "rotation": 0,
+          "target": "1-pano_20250513_180718_9"
+        },
+        {
+          "yaw": 2.228561649933382,
+          "pitch": 0.17674056728215248,
+          "rotation": 0,
+          "target": "2-pano_20250513_182301_11"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.600832296932291,
+          "pitch": 0.009749018362574802,
+          "title": "垃圾廚房",
+          "text": "「垃圾廚坊Trash Kitchen」以簡單的「垃圾」料理模式，帶領大家了解循環再造，創造產品並賦予回收材料新價值，讓循環經濟融入大眾的日常。垃圾廚坊Trash Kitchen如同走入廚房，打開五感來動手體驗與學習並揮創意，將垃圾作為食材，料理出一道名為循環的菜餚。透明化的過程，讓大家親自體驗發掘我們常見的一次性垃圾化廢為寶可以這麼簡單快速！"
+        }
+      ]
+    },
+    {
+      "id": "1-pano_20250513_180718_9",
+      "name": "PANO_20250513_180718_9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.09788268617386287,
+        "pitch": 0.4010394066948244,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7648467634515335,
+          "pitch": 0.28925378720038886,
+          "rotation": 0,
+          "target": "2-pano_20250513_182301_11"
+        },
+        {
+          "yaw": 3.0410458728483727,
+          "pitch": 0.13278653669731355,
+          "rotation": 0,
+          "target": "0-pano_20250513_172052_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.017188336742838928,
+          "pitch": 0.24296360901332292,
+          "title": "非常有藝術感且可以看像巴洛克花園的窗戶",
+          "text": "外面就是直對著巴洛克花園，可以在階梯這邊休息看風景"
+        }
+      ]
+    },
+    {
+      "id": "2-pano_20250513_182301_11",
+      "name": "PANO_20250513_182301_11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.341398027277494,
+        "pitch": 0.027539616930701172,
+        "fov": 1.4628963779807613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8343611227301793,
+          "pitch": 0.018024892573910378,
+          "rotation": 0,
+          "target": "0-pano_20250513_172052_2"
+        },
+        {
+          "yaw": 1.6954067402781785,
+          "pitch": 0.014012667438905169,
+          "rotation": 0,
+          "target": "1-pano_20250513_180718_9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7947738484944136,
+          "pitch": 0.16736039065808583,
+          "title": "最近的活動",
+          "text": "這個展區是定期更換的"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
