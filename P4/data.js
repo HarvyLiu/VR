08c46022a68dc60062ce6ed9abe3d8adc780f080
HarvyLiu <harvyliu2009@gmail.com>
2025-05-20 09:23:26 +0800

    P4

---
+++ P4/data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "步道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.198014418741714,
+        "pitch": 0.14492282251198674,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9327008068400389,
+          "pitch": 0.01643735035741578,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4595867007629675,
+          "pitch": 0.14749878716514786,
+          "title": "生態池前步道",
+          "text": "一個讓人心曠神怡的步道，非常的舒適自在。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "觀賞台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.01415126225723995,
+        "pitch": 0.1843589308590161,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8312908893075495,
+          "pitch": 0.013848630232054404,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16026756614489202,
+          "pitch": 0.16420298109797926,
+          "title": "生態池",
+          "text": "生態景觀池為大面積的人造蓄水池，在菸廠時期作為消防目的使用，現今周邊保留多樣保育類植物，為臺北市少見的城市濕地，堪稱臺北東區的綠寶石。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
